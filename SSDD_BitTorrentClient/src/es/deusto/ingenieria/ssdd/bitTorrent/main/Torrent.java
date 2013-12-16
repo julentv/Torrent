9954cf11c110dfc04863d75586e63903ab0f4a11
Julen Windows <julentv@gmail.com>
2013-12-16 17:49:09 +0100

    nuevo torrent

--- SSDD_BitTorrentClient/src/es/deusto/ingenieria/ssdd/bitTorrent/main/Torrent.java
+++ SSDD_BitTorrentClient/src/es/deusto/ingenieria/ssdd/bitTorrent/main/Torrent.java
@@ -13,17 +13,32 @@ import es.deusto.ingenieria.ssdd.bitTorrent.metainfo.MetainfoFile;
 import es.deusto.ingenieria.ssdd.bitTorrent.metainfo.handler.MetainfoFileHandler;
 import es.deusto.ingenieria.ssdd.bitTorrent.metainfo.handler.MultipleFileHandler;
 import es.deusto.ingenieria.ssdd.bitTorrent.metainfo.handler.SingleFileHandler;
+import es.deusto.ingenieria.ssdd.bitTorrent.util.ToolKit;
 
 public class Torrent {
+	private String peerId;
+	private int port;
+	
+	public Torrent(){
+		this.peerId=ToolKit.generatePeerId();
+		this.port=8888;
+	}
+	public void dowloadTorrent(String torrentName) throws IOException{
+		Torrent torrent= new Torrent();
+		MetainfoFile<?> metainf=torrent.obtainMetaInfo(torrentName);
+		System.out.println(metainf.toString());
+		httprRequest("", metainf);
+	}
 
 	public static void main(String[] args) throws IOException {
 		System.out.print("Insert the name of the torrent: ");
 		BufferedReader reader = new BufferedReader(new InputStreamReader(
 				System.in));
-		String torrentFileName = reader.readLine();
+		//String torrentFileName = reader.readLine();
+		String torrentFileName = "SAN IGNACIO.docx.torrent";
 		Torrent torrent= new Torrent();
-		System.out.println(torrent.obtainMetaInfo(torrentFileName).toString());
-		torrent.httprRequest("");
+		torrent.dowloadTorrent(torrentFileName);
+
 
 	}
 	/**
@@ -43,8 +58,9 @@ public class Torrent {
 		}
 		return handler.getMetainfo();
 	}
-	public void httprRequest(String urlText) throws IOException{
-		urlText="http://ipv6.torrent.ubuntu.com:6969/announce?info_hash=E3811B9539CACFF680E418124272177C47477157&peer_id=15&port=8080&uploaded=0&downloaded=0&left=524288&event=started";
+	public void httprRequest(String urlText, MetainfoFile<?> metainf) throws IOException{
+		urlText="http://10.172.203.192:48643/announce?info_hash="+metainf.getInfo().getUrlInfoHash()+"&peer_id="+this.peerId+"&port=8080&uploaded=0&downloaded=0&left=16384&event=started";
+		System.out.println(urlText);
 		//urlText="http://google.com";
 		String USER_AGENT = "Mozilla/5.0"; 
 		URL obj = new URL(urlText);
